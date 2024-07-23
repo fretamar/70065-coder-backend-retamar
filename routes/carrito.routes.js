487841c054c187400a9ed453ffbbbe2570df801c
Francisco Retamar <franretamar123@gmail.com>
2024-07-22 21:07:53 -0300

    realice correcciones de pre-entrega1

--- routes/carrito.routes.js
+++ routes/carrito.routes.js
@@ -1,117 +1,103 @@
-import { Router } from 'express';
-import fs from 'fs';
-const carritoRouter = Router();
+import { Router } from 'express'
+import fs from 'fs'
+const carritoRouter = Router()
 
-// Ruta para crear un nuevo carrito
 
 carritoRouter.post('/carts', (req, res) => {
-    const { id, product } = req.body;
+    const { id, product } = req.body
     if (id && product) {
-
-        // Escribir el carrito en el archivo "carrito.json"
 
         fs.readFile('../backend/src/carrito.json', 'utf-8', (err, data) => {
             if (err) {
-                console.error(err);
-                return res.status(500).json({ error: 'Error interno de servidor' });
+                console.error(err)
+                return res.status(500).json({ error: 'Error interno de servidor' })
             }
 
-            const carts = JSON.parse(data);
-            carts.push({ id, product });
+            const carts = JSON.parse(data)
+            carts.push({ id, product })
 
         fs.writeFile('../backend/src/carrito.json', JSON.stringify(carts, null, 2), err => {
             if (err) {
-                console.error(err);
-                return res.status(500).json({ error: 'Error interno de servidor' });
+                console.error(err)
+                return res.status(500).json({ error: 'Error interno de servidor' })
             }
-            res.json({ id, product });
-        });
-    });
+            res.json({ id, product })
+        })
+    })
 } else {
-    res.status(400).json({ error: 'Se requiere ID y Producto' });
+    res.status(400).json({ error: 'Se requiere ID y Producto' })
 }
-});
-
-// Ruta para obtener los productos de carrito
+})
 
 
 carritoRouter.get('/carts', (req, res) => {
     fs.readFile('../backend/src/carrito.json', 'utf-8', (err, data) => {
         if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Error interno del servidor' });
+            console.error(err)
+            return res.status(500).json({ error: 'Error interno del servidor' })
         }
 
-        const cart = JSON.parse(data);  
-        const limit = req.query.limit;
+        const cart = JSON.parse(data)  
+        const limit = req.query.limit
 
         if (limit) {
-            res.json(cart.slice(0, limit));
+            res.json(cart.slice(0, limit))
         } else {
-            res.json(cart);
+            res.json(cart)
         }
-    });
-});
-
-// Ruta para obtener un carrito por su ID
+    })
+})
 
 carritoRouter.get('/carts/:cid', (req, res) => {
-    const id = req.params.cid;
+    const id = req.params.cid
 
-    // Leer el archivo "carrito.json"
     
     fs.readFile('../backend/src/carrito.json', 'utf-8', (err, data) => {
         if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Error interno de servidor' });
+            console.error(err)
+            return res.status(500).json({ error: 'Error interno de servidor' })
         }
-        const carts = JSON.parse(data);
-        const cart = carts.find(cart => cart.id ===parseInt(id));
+        const carts = JSON.parse(data)
+        const cart = carts.find(cart => cart.id ===parseInt(id))
         if (cart) {
-            res.json(cart);
+            res.json(cart)
         } else {
-            res.status(404).json({ error: 'Carrito no encontrado' });
+            res.status(404).json({ error: 'Carrito no encontrado' })
         }
-    });
-});
-
-// Ruta para agregar un producto a un carrito
+    })
+})
 
 carritoRouter.post('/:cid/product/:pid', (req, res) => {
-    const cartId = req.params.cid;
-    const productId = req.params.pid;
+    const cartId = req.params.cid
+    const productId = req.params.pid
     const productToAdd = {
         product: productId,
         quantity: 1
-    };
-
-    // Leer el archivo "carrito.json"
+    }
 
     fs.readFile('../backend/src/carrito.json', 'utf-8', (err, data) => {
         if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Error interno de servidor' });
+            console.error(err)
+            return res.status(500).json({ error: 'Error interno de servidor' })
         }
 
-        const carts = JSON.parse(data);
-        const cartIndex = carts.findIndex(cart => cart.id === cartId);
+        const carts = JSON.parse(data)
+        const cartIndex = carts.findIndex(cart => cart.id === cartId)
 
         if (cartIndex !== -1) {
-            carts[cartIndex].products.push(productToAdd);
-
-            // Escribir los cambios en el archivo "carrito.json"
+            carts[cartIndex].products.push(productToAdd)
 
             fs.writeFile('../backend/src/carrito.json', JSON.stringify(carts, null, 2), err => {
                 if (err) {
-                    console.error(err);
-                    return res.status(500).json({ error: 'Error interno de servidor' });
+                    console.error(err)
+                    return res.status(500).json({ error: 'Error interno de servidor' })
                 }
-                res.status(200).json({ message: 'Producto agregado al carrito exitosamente' });
-            });
+                res.status(200).json({ message: 'Producto agregado al carrito exitosamente' })
+            })
         } else {
-            res.status(404).json({ error: 'Carrito no encontrado' });
+            res.status(404).json({ error: 'Carrito no encontrado' })
         }
-    });
-});
+    })
+})
 
 export default carritoRouter
--- routes/productos.routes.js
+++ routes/productos.routes.js
@@ -1,86 +1,140 @@
 import { Router } from 'express';
 import fs from 'fs';
-const productosRouter = Router();
+import { body, validationResult } from 'express-validator'
+const productosRouter = Router()
 
 productosRouter.get('/products', (req, res) => {
     fs.readFile('../backend/src/productos.json', 'utf-8', (err, data) => {
         if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Error interno del servidor' });
+            console.error(err)
+            return res.status(500).json({ error: 'Error interno del servidor' })
         }
 
-        const products = JSON.parse(data);  
-        const limit = req.query.limit;
+        const products = JSON.parse(data)
+        const limit = req.query.limit
 
         if (limit) {
-            res.json(products.slice(0, limit));
+            res.json(products.slice(0, limit))
         } else {
-            res.json(products);
+            res.json(products)
         }
     });
 });
 
 productosRouter.get('/products/:pid', (req, res) => {
-    const id = req.params.pid;
+    const id = req.params.pid
 
     fs.readFile('../backend/src/productos.json', 'utf-8', (err, data) => {
         if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Error interno del servidor' });
+            console.error(err)
+            return res.status(500).json({ error: 'Error interno del servidor' })
         }
 
-        const products = JSON.parse(data);
-        const product = products.find(product => product.id === parseInt(id));
+        const products = JSON.parse(data)
+        const product = products.find(product => product.id === parseInt(id))
 
         if (product) {
-            res.json(product);
+            res.json(product)
         } else {
-            res.status(404).json({ error: 'Producto no encontrado' });
+            res.status(404).json({ error: 'Producto no encontrado' })
         }
-    });
-});
+    })
+})
 
-productosRouter.post('/products', (req, res) => {
-    const { title, description, price, status, stock, category } = req.body;
+productosRouter.post('/products', [
+    body('title').notEmpty().withMessage('El título es obligatorio'),
+    body('description').notEmpty().withMessage('La descripción es obligatoria'),
+    body('price').isNumeric().withMessage('El precio debe ser un número'),
+    body('stock').isNumeric().withMessage('El stock debe ser un número'),
+    body('category').notEmpty().withMessage('La categoría es obligatoria')
+], (req, res) => {
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+        return res.status(400).json({ errors: errores.array() })
+    }
+
+    const { title, description, price, status, stock, category } = req.body
+    const newStatus = status || true
 
     fs.readFile('../backend/src/productos.json', 'utf-8', (err, data) => {
         if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Error interno del servidor' });
+            console.error(err)
+            return res.status(500).json({ error: 'Error interno del servidor' })
         }
 
-        const products = JSON.parse(data);
-        const id = products.length + 1;
-        const newProduct = { id, title, description, price, status, stock, category };
-        products.push(newProduct);
+        const products = JSON.parse(data)
+        const id = products.length > 0 ? products[products.length - 1].id + 1 : 1
+        const newProduct = { id, title, description, price, status: newStatus, stock, category }
+        products.push(newProduct)
 
         fs.writeFile('../backend/src/productos.json', JSON.stringify(products, null, 2), err => {
             if (err) {
-                console.error(err);
-                return res.status(500).json({ error: 'Error interno del servidor' });
+                console.error(err)
+                return res.status(500).json({ error: 'Error interno del servidor' })
             }
-            res.json(newProduct);
+            res.json(newProduct)
+        })
+    })
+})
 
-        });
-    });
-});
-
-productosRouter.delete('/products/:pid', (req, res) => {
-    const id = req.params.pid;
+productosRouter.put('/products/:pid', (req, res) => {
+    const productId = parseInt(req.params.pid)
 
     fs.readFile('../backend/src/productos.json', 'utf-8', (err, data) => {
         if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Error interno del servidor' });
+            console.error(err)
+            return res.status(500).json({ error: 'Error interno del servidor' })
         }
-        const products = JSON.parse(data);
-        const product = products.filter(product => product.id !== parseInt(id));
-        if (product) {
-            res.json(product);
+
+        let products = JSON.parse(data)
+        const product = products.find(product => product.id === productId)
+        if (product !== -1) {
+            const { title, description, price, stock, category } = req.body
+            products[product] = {
+                ...products[product],
+                title,
+                description,
+                price,
+                stock,
+                category
+            }
+            fs.writeFile('../backend/src/productos.json', JSON.stringify(products, null, 2), err => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).json({ error: 'Error interno del servidor' })
+                }
+                res.json(products[product])
+            })
         } else {
-            res.status(404).json({ error: 'Producto no encontrado' });
+            res.status(404).json({ message: "Producto no encontrado" })
         }
-    });
-});
+    })
+})
+
+productosRouter.delete('/products/:pid', (req, res) => {
+    const id = req.params.pid
+
+    fs.readFile('../backend/src/productos.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return res.status(500).json({ error: 'Error interno del servidor' })
+        }
+        const products = JSON.parse(data)
+        const product = products.filter(product => product.id !== parseInt(id))
+        if (product < products) {
+            res.json(product)
+        } else {
+            res.status(404).json({ error: 'Producto no encontrado' })
+        }
+
+        fs.writeFile('../backend/src/productos.json', JSON.stringify(product, null, 2), err => {
+            if (err) {
+                console.error(err)
+                return res.status(500).json({ error: 'Error interno del servidor' })
+            }
+            res.json(product)
+        })
+    })
+})
 
 export default productosRouter
